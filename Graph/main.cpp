886eb8d1f7da50858f7bb6e35b786c23fea3bf21
KismusGrine <58406658+KismusGrine@users.noreply.github.com>
2019-12-09 05:54:45 +0300

    Update main.cpp
    Переделал функцию создания связей

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <fstream>
+#include <queue>
 
 using namespace std;
 
+struct Node {
+    int vertex;
+    unsigned int len;
+};
+
 class Graph {
     // Список смежности
-    vector <list<int>> Vertex;
+    vector<list<int>> Vertex;
 
 // Количество вершин и ребер/дуг
     int count_vertex;
@@ -18,7 +25,7 @@ public:
 
     Graph(int num);
 
-    Graph(int **A, int num);
+    Graph(int **matrix, int num);
 
     void Add_vertex();
 
@@ -27,11 +34,11 @@ public:
     void Delete_vertex();
 
     // Сделать
-    void Delete_edge();
+    void Delete_edge(int s, int f);
 
-    void bfs();
+    unsigned int bfs(int start, int finish);
 
-    void dfs();
+    unsigned int dfs();
 };
 
 Graph::Graph() {
@@ -48,17 +55,79 @@ Graph::Graph(int num) {
         Vertex.push_back(ver);
 }
 
-Graph::Graph(int **A, int num) {
-    count_vertex = num;
+Graph::Graph(int **matrix, int num) {
+    count_vertex = 0;
     count_edge = 0;
+
+    for (int i = 0; i < num; i++) {
+        Add_vertex();
+    }
+
+    for (int i = 0; i < num; i++) {
+        for (int j = i + 1; j < num; j++) {
+            if (matrix[i][j] == 1) {
+                Add_edge(i, j); //функция добавляет ребро и в ту и в другую сторону, увеличивает количество ребер
+            }
+        }
+    }
+} //в коментах есть вопросы
+
+void Graph::Add_vertex() {
+    list<int> ver; //Пустой лист?
+    Vertex.push_back(ver);
+    count_vertex++;
 }
 
-void Graph::Delete_edge() {
+void Graph::Add_edge(int s, int f) {
+    if (Vertex.size() >= s && Vertex.size() >= f) { 
+        Vertex[s-1].push_back(f);
+        count_edge++;
+    }
+} // Может так сделать? Не будет дубликатов в листах и будет правильный список смежности
 
+// TODO пересмотреть
+void Graph::Delete_edge(int s, int f) {
+    if (Vertex.size() >= s) {
+        for (auto iter = Vertex[s].begin(); iter != Vertex[s].end(); iter++) {
+            if (*iter == f)
+                iter = Vertex[s].erase(iter);
+        }
+    }
+
+    if (Vertex.size() >= f) {
+        for (auto iter = Vertex[s].begin(); iter != Vertex[s].end(); iter++) {
+            if (*iter == s)
+                iter = Vertex[s].erase(iter);
+        }
+    }
+}
+
+unsigned int Graph::bfs(int start, int finish) {
+    queue<Node> q;
+    Node tmp = {start, 0};
+    q.push(tmp);
+
+    while (!q.empty()) {
+        tmp = q.front();
+        tmp.len++;
+        q.pop();
+        int vert = tmp.vertex;
+
+
+        for (auto iter = Vertex[vert].begin(); iter != Vertex[vert].end(); iter++) {
+            if (*iter == finish) {
+                return tmp.len;
+            }
+
+            tmp.vertex = *iter;
+            q.push(tmp);
+        }
+    }
+
+    return -1;
 }
 
 int main() {
-
-
+    cout << "Hello, world" << endl;
     return 0;
 }
